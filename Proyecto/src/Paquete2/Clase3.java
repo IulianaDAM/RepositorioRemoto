233d39527da6f7b829f19c729cc80a82df0b5150
nidana <nidana@DESKTOP-CRJ5T8P>
2024-02-25 22:38:02 +0100

    Commit

---
+++ Proyecto/src/Paquete2/Clase3.java
@@ -0,0 +1,13 @@
+package Paquete2;
+
+public class Clase3 {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("AAAAAAA");
+		System.out.println("klk");
+		System.out.println("AAAAA");
+	}
+
+}
